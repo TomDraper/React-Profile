b23d0c4fdbfdf7793c005abd58e8cad23504da0c
TomDraper <mythirdalias@gmail.com>
2026-04-11 23:13:15 +0100

    Main Page, Routing and some about page transferred over

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import './css/vars.css';
+
+
+import HomePage from "./pages/home.tsx";
+import AboutPage from "./pages/about.tsx";
+
+const homePage = HomePage();
+const aboutPage = AboutPage();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          More testing is needed I think.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element = {homePage} />
+        <Route path="/about" element={aboutPage} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { BrowserRouter } from 'react-router-dom';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+      <App />
   </React.StrictMode>
 );
 
@@ -17,3 +19,6 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+    
+
