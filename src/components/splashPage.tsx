8cf796d27594f8e3a2d8dae16a7e5cc3ca1bb821
TomDraper <mythirdalias@gmail.com>
2026-05-06 16:25:13 +0100

    Fixed a few bugs with the terminal stuff and the splash page links.

--- src/components/splashPage.tsx
+++ src/components/splashPage.tsx
@@ -8,9 +8,10 @@ export default function SplashPage(){
             <div className="splashPageVignette center">
                 <h2>Tom Draper</h2>
                 <h1 className="greenText">Games Programmer</h1>
-                <h3>Creating immersive gaming experiences, improving tooling to enable faster development and polishing with pixel perfect UI designed for a seamless user experience.</h3>
+                <h3>Creating immersive gaming experiences, improving tooling to enable faster development and polishing with pixel perfect UI designed for a seamless user experience. <p>Created using React.</p></h3>
+                
                 <div className="horizontalContainer center">
-                    <a href="projects.html"><button className="btn greenButton">View Projects</button></a><a href="contact.html"><button className="btn transparentButton">Get in Touch</button></a>
+                    <a href="#/projects"><button className="btn greenButton">View Projects</button></a><a href="#/contact"><button className="btn transparentButton">Get in Touch</button></a><a href="#/terminalGame"><button className="btn transparentButton">Terminal</button></a>
                 </div>
             </div>
         </div>
--- src/pages/terminalGamePage.tsx
+++ src/pages/terminalGamePage.tsx
@@ -2,7 +2,6 @@ import NavBar from '../components/navbar';
 import TerminalWindow from '../components/terminalWindow';
 
 export default function TerminalGame(){
-    console.log("opened terminal page");
     return (
         <div>
             <NavBar selected={4}/>
