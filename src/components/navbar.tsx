45ed41cecbdb00d82efd06ec5a0c83495cd65174
TomDraper <mythirdalias@gmail.com>
2026-05-15 17:26:07 +0100

    Nav bar is now mobile friendly and won't create awkward spacing.

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom';
 
+import navIcon from '../images/icons/Menu.png'
+
 type NavBarLinkProp = {
     current: boolean,
     text: string,
@@ -18,22 +20,47 @@ export default function NavBar({selected}:{selected:number}){
     for(let i = 0; i < navBarLinks.length; i++){
         navBarLinks[i].current = selected === i;
     }
+
     return (
-        <div className="navBar">
+        <div id="navBar" className="navBar">
+            <div className="navBarHeader">
+                <p className="greenText title">Tom Draper</p>
+                <div className="dropdownIconContainer">
+                    <a onClick={displayMobileNav}>
+                        <img className="dropdownIcon" src={navIcon}></img>
+                    </a>
+                </div>
+            </div>
             <ul>
-                <li><p className="greenText title">Tom Draper</p></li>
                 <NavBarLinks props={navBarLinks} />
             </ul>
         </div>
     );
 }
 
-export function NavBarLinks({ props } : { props:NavBarLinkProp[] }){
+function NavBarLinks({ props } : { props:NavBarLinkProp[] }){
     return props.map((prop, index) => (
         <NavBarLink key={index} {...prop} />
     ));
 }
 
 function NavBarLink({ current, text, url }: NavBarLinkProp){
-    return <li><Link className={current ? "greenText" : "greyText"} to={url}>{text}</Link></li>;
+    return <li><Link className={current ? "greenText" : "greyText"} to={url} onClick={hideMobileNav}>{text}</Link></li>;
+}
+
+function displayMobileNav() {
+    var x = document.getElementById("navBar");
+    if (x == null) return;
+
+    if (x.className === "navBar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navBar";
+    }
+}
+
+function hideMobileNav() {
+    var x = document.getElementById("navBar");
+    if (x == null) return;
+    x.className = "navBar";
 }
