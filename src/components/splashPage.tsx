ffae0daf4c74836f1d454980722a6a6ec755ed8a
TomDraper <mythirdalias@gmail.com>
2026-09-02 13:11:53 +0100

    Changes to fade in view and added link to source code on main page

--- src/components/splashPage.tsx
+++ src/components/splashPage.tsx
@@ -8,7 +8,10 @@ export default function SplashPage(){
             <div className="splashPageVignette center">
                 <h2>Tom Draper</h2>
                 <h1 className="greenText">Games Programmer</h1>
-                <h3>Creating immersive gaming experiences, improving tooling to enable faster development and polishing with pixel perfect UI designed for a seamless user experience. <p>Created using React.</p></h3>
+                <h3>Creating immersive gaming experiences, improving tooling to enable faster development and polishing with pixel perfect UI designed for a seamless user experience. 
+                    <p>Created using React.
+                    <br/><a href="https://github.com/TomDraper/React-Profile/tree/master">Source Code</a></p>
+                </h3>
                 
                 <div className="horizontalContainer center">
                     <a href="#/projects"><button className="btn greenButton">View Projects</button></a><a href="#/contact"><button className="btn transparentButton">Get in Touch</button></a><a href="#/terminalGame"><button className="btn transparentButton">Terminal</button></a>
